e82e4b20fcda07b5e6bcb55de71bbbc030291ac4
Daniel-VM <daniel.vergara@dfinsolutions.com>
2024-01-22 18:01:33 -0500

    reordering document to make it easy to read

--- Tests/HackerRankUTest.cs
+++ Tests/HackerRankUTest.cs
@@ -1,23 +1,40 @@
 using AutoFixture;
 using HackerRankLib;
 using HackerRankLib.Model;
-using System.Linq;
 using Xunit.Abstractions;
 
 namespace Tests
 {
     public class HackerRankUTest
     {
-        private readonly ITestOutputHelper _testOutputHelper;
         private readonly Fixture _fixture;
-
+        private readonly ITestOutputHelper _testOutputHelper;
         public HackerRankUTest(ITestOutputHelper testOutputHelper)
         {
             HackerRankLibHelper.Initialize(new HackerRankLibrary());
             _testOutputHelper = testOutputHelper;
             _fixture = new Fixture();
         }
-        
+
+        public static IEnumerable<object[]> TreeTestData =>
+            new List<object[]>
+            {
+                new object[] { new Tree() { Value = 1, LeftTree = new Tree { Value = 2, LeftTree = new Tree { Value = 5, LeftTree = new Tree { Value = 3, LeftTree = null, RightTree = null }, RightTree = null }, RightTree = new Tree { Value = 9, LeftTree = null, RightTree = null } }, RightTree = new Tree { Value = 7, LeftTree = new Tree { Value = 4, LeftTree = null, RightTree = new Tree { Value = 0, LeftTree = null, RightTree = null } }, RightTree = new Tree { Value = 8, LeftTree = null, RightTree = null } } }, 1 },
+                new object[] { new Tree() { Value = 1, LeftTree = new Tree { Value = 2, LeftTree = new Tree { Value = 5, LeftTree = new Tree { Value = 3, LeftTree = null, RightTree = null }, RightTree = null }, RightTree = new Tree { Value = 9, LeftTree = null, RightTree = null } }, RightTree = new Tree { Value = 7, LeftTree = new Tree { Value = 4, LeftTree = null, RightTree = new Tree { Value = 0, LeftTree = null, RightTree = null } }, RightTree = new Tree { Value = 8, LeftTree = null, RightTree = null } } }, 2 },
+                new object[] { new Tree() { Value = 1, LeftTree = new Tree { Value = 2, LeftTree = new Tree { Value = 5, LeftTree = new Tree { Value = 3, LeftTree = null, RightTree = null }, RightTree = null }, RightTree = new Tree { Value = 9, LeftTree = null, RightTree = null } }, RightTree = new Tree { Value = 7, LeftTree = new Tree { Value = 4, LeftTree = null, RightTree = new Tree { Value = 0, LeftTree = null, RightTree = null } }, RightTree = new Tree { Value = 8, LeftTree = null, RightTree = null } } }, 3 },
+            };
+
+        [Fact]
+
+        public void BetterCompressionSuccess()
+        {
+            const string original = "a2b3c4a5b4c3d7";
+            const string expected = "a7b7c7d7";
+            var answer = HackerRankLibHelper.BetterCompression(original);
+            _testOutputHelper.WriteLine(answer);
+            Assert.Equal(expected, answer);
+        }
+
         [Theory]
         [InlineData(12345)]
         public void IsPalindromeFailure(int number)
@@ -44,46 +61,6 @@ namespace Tests
             _testOutputHelper.WriteLine(answer);
             Assert.NotEmpty(answer);
         }
-        
-        [Theory]
-        [InlineData(5)]
-
-        public void StairCaseSuccess(int number)
-        {
-            var answer = HackerRankLibHelper.StairCase(number);
-            _testOutputHelper.WriteLine(answer);
-            Assert.NotEmpty(answer);
-        }
-
-        [Fact]
-        
-        public void BetterCompressionSuccess()
-        {
-            const string original = "a2b3c4a5b4c3d7";
-            const string expected = "a7b7c7d7";
-            var answer = HackerRankLibHelper.BetterCompression(original);
-            _testOutputHelper.WriteLine(answer);
-            Assert.Equal(expected, answer);
-        }
-
-        [Theory]
-        [InlineData(25,9)]
-        
-        public void PossibleTwoSumsFailure(int target, int arrayLength)
-        {
-            var answer = HackerRankLibHelper.PossibleTwoSums( target, arrayLength);
-            _testOutputHelper.WriteLine(answer);
-            Assert.Empty(answer);
-        }
-        [Theory]
-        [InlineData(13, 9)]
-
-        public void PossibleTwoSumsSuccess(int target, int arrayLength)
-        {
-            var answer = HackerRankLibHelper.PossibleTwoSums( target, arrayLength);
-            _testOutputHelper.WriteLine(answer);
-            Assert.NotEmpty(answer);
-        }
 
         [Theory]
         [MemberData(nameof(TreeTestData))]
@@ -95,13 +72,34 @@ namespace Tests
             Assert.NotEmpty(answer.ToString());
         }
 
-        public static IEnumerable<object[]> TreeTestData =>
-            new List<object[]>
-            {
-                new object[] { new Tree() { Value = 1, LeftTree = new Tree { Value = 2, LeftTree = new Tree { Value = 5, LeftTree = new Tree { Value = 3, LeftTree = null, RightTree = null }, RightTree = null }, RightTree = new Tree { Value = 9, LeftTree = null, RightTree = null } }, RightTree = new Tree { Value = 7, LeftTree = new Tree { Value = 4, LeftTree = null, RightTree = new Tree { Value = 0, LeftTree = null, RightTree = null } }, RightTree = new Tree { Value = 8, LeftTree = null, RightTree = null } } }, 1 },
-                new object[] { new Tree() { Value = 1, LeftTree = new Tree { Value = 2, LeftTree = new Tree { Value = 5, LeftTree = new Tree { Value = 3, LeftTree = null, RightTree = null }, RightTree = null }, RightTree = new Tree { Value = 9, LeftTree = null, RightTree = null } }, RightTree = new Tree { Value = 7, LeftTree = new Tree { Value = 4, LeftTree = null, RightTree = new Tree { Value = 0, LeftTree = null, RightTree = null } }, RightTree = new Tree { Value = 8, LeftTree = null, RightTree = null } } }, 2 },
-                new object[] { new Tree() { Value = 1, LeftTree = new Tree { Value = 2, LeftTree = new Tree { Value = 5, LeftTree = new Tree { Value = 3, LeftTree = null, RightTree = null }, RightTree = null }, RightTree = new Tree { Value = 9, LeftTree = null, RightTree = null } }, RightTree = new Tree { Value = 7, LeftTree = new Tree { Value = 4, LeftTree = null, RightTree = new Tree { Value = 0, LeftTree = null, RightTree = null } }, RightTree = new Tree { Value = 8, LeftTree = null, RightTree = null } } }, 3 },
-            };
-        
+        [Theory]
+        [InlineData(25, 9)]
+
+        public void PossibleTwoSumsFailure(int target, int arrayLength)
+        {
+            var answer = HackerRankLibHelper.PossibleTwoSums(target, arrayLength);
+            _testOutputHelper.WriteLine(answer);
+            Assert.Empty(answer);
+        }
+
+        [Theory]
+        [InlineData(13, 9)]
+
+        public void PossibleTwoSumsSuccess(int target, int arrayLength)
+        {
+            var answer = HackerRankLibHelper.PossibleTwoSums(target, arrayLength);
+            _testOutputHelper.WriteLine(answer);
+            Assert.NotEmpty(answer);
+        }
+
+        [Theory]
+        [InlineData(5)]
+
+        public void StairCaseSuccess(int number)
+        {
+            var answer = HackerRankLibHelper.StairCase(number);
+            _testOutputHelper.WriteLine(answer);
+            Assert.NotEmpty(answer);
+        }
     }
 }
