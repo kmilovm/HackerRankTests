726f3efd6309f7ca59bd7aa117f4c76a28cc24ad
Daniel-VM <daniel.vergara@dfinsolutions.com>
2024-01-24 10:23:14 -0500

    Improving the tests and introducing wrong case scenario given on online test

--- HackerRankLib/obj/Debug/net6.0/HackerRankLib.AssemblyInfo.cs
+++ HackerRankLib/obj/Debug/net6.0/HackerRankLib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HackerRankLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+772c628a0d248f018b565f51c4d0d83865cf39ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1226be5037354147846eb9af422920cbf9be7b56")]
 [assembly: System.Reflection.AssemblyProductAttribute("HackerRankLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HackerRankLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
